b8ebe8a937d96396912dfc55b1acc875dad923d6
Zanzan666 <34534797+Zanzan666@users.noreply.github.com>
2018-01-01 19:52:19 +0800

    work

--- a2project1/README.md
+++ a2project1/README.md
@@ -1,15 +1,31 @@
-﻿# Group Work
-## Jenny Zhan & Laurel Kuang
+## Group Work
+*By:  Jenny Zhan & Laurel Kuang*
 
-**Topic: Room Escape**
-1. title: 
-2. setting: in ICC building
-3. 
-class people():
-class rooms():
-class personalInfo():
+**Topic: Sketching Graphs in Mathematics**
+#### 1. title: Graph Sketcher
+#### 2. function: Sketching a graph in Further Maths
+#### 3. before you start:
 
-6 floors, ? rooms
+Please [install matplotlib](https://matplotlib.org/users/installing.html)
 
-1 person
-1 bad person(NPC)
+#### 4. modules overview
+```python
+class AskForEquation():
+	def __init__()
+	def setVariables():
+	def submit():
+	def translate():
+class draw():
+	def __init__():
+	def defineEquation():
+	def draw():
+```
+
+#### 5. what's for?
+
+The idea of this program comes from sketching graphs in Further Maths. In Further Maths exams, we always encounter questions that require us to sketch a complicated graph, and drawing it point by point takes us too much time. Thus, we design this program, helping us to sketch the graph in a quicker and more accurate way. 
+
+By this program, we can just follow the format given and input the coefficients of our equation. Then, the graph will be printed, while the angles and coordinates related to the graph will be noted as well. This program can be used when solving questions in Further Maths and other mathematics problems, and it will improve our speed of solving problems and reduce mistakes we tend to make while sketching graphs.
+
+In the future development, we plan to improve this program in a variety ways. For instance, we will allow more equations to be input at the same time, and we will set up a table to show important coordinates of the graph. Besides, we will try to show the graph in the form of Cartesian Coordinates. Meanwhile, we plan to enhance the graphical user interface and add some dynamics to show changes in graphs.
+ 
---
+++ a2project1/S3work.py
@@ -0,0 +1,64 @@
+#Jenny&Laurel
+import numpy as np
+import matplotlib.pyplot as plt
+from tkinter import*
+from tkinter.messagebox import showinfo
+class AskForEquation():
+    def __init__(self):
+        self.root=Tk()
+        self.root.title('Enter your equation')
+        self.l=[[]for i in range(6)]
+        self.e=[[]for i in range(6)]
+        self.text=[[]for i in range(6)]
+        self.translatedText=[[]for i in range(6)]
+        self.setVariables(0)
+        self.equationInfo1=Label(self.root,text='r=a+b*sin(c*θ) or r=a+b*cos(c*θ), or r=a+b+c*θ,')
+        self.equationInfo1.grid(row=0,sticky=W)
+        self.equationInfo2=Label(self.root,text='d = "sin" or "cos" or "direct"')
+        self.equationInfo2.grid(row=1,sticky=W)
+        self.equationInfo3=Label(self.root,text='e*pi ≤ θ ≤ f*pi')
+        self.equationInfo3.grid(row=2,sticky=W)
+        self.doneButton = Button(self.root,text='Done',command=self.submit)
+        self.doneButton.grid(row=9,column=1,sticky=E)
+    def setVariables(self,k):
+        if k>5:
+            return
+        self.text[k]=StringVar()
+        self.l[k]=Label(self.root,text=chr(97+k))
+        self.l[k].grid(row=k+3,sticky=W)
+        self.e[k]=Entry(self.root,textvariable=self.text[k])
+        self.e[k].grid(row=k+3,column=1,sticky=E)
+        self.setVariables(k+1)
+    def submit(self):
+        if self.text[3].get()=="cos" or self.text[3].get()=="sin" or self.text[3].get()=="direct":
+            self.translate()
+        else:
+            showinfo('Wrong',"Invalid Input")
+    def translate(self,k=5):
+        if k<0:
+            draw(self.translatedText)
+            return
+        try:self.translatedText[k]=int(self.text[k].get())
+        except:self.translatedText[k]=self.text[k].get()
+        self.translate(k-1)
+        
+
+class draw():
+    def __init__(self,x):
+        self.ax=plt.subplot(111, projection='polar')
+        self.defineEquation(x)
+        self.draw()
+    def defineEquation(self,x):
+        self.theta=np.arange(int(x[4])*np.pi,int(x[5])*np.pi,0.001)
+        if x[3]=='cos':
+            self.r=x[0]+x[1]*np.cos(x[2]*self.theta)
+        if x[3]=='sin':
+            self.r=x[0]+x[1]*np.sin(x[2]*self.theta)
+        if x[3]=='direct':
+            self.r=x[0]+x[1]*x[2]*self.theta
+    def draw(self):
+        self.ax.plot(self.theta, self.r)
+        self.ax.grid(True)
+        plt.show()
+
+AskForEquation()
